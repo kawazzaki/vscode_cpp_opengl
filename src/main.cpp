ef31e4e91e5539679b544cfb74c5a5b4e3cc8130
kawazzaki98 <nachreda02@gmail.com>
2025-11-30 21:57:40 +0100

    finish 2d

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,23 @@
 #include "VAO.h"
 #include "EBO.h"
 #include "VBO.h"
+#include "texture.h"
+
+GLfloat vertices[] =
+    {
+        //     COORDINATES     /        COLORS      /   TexCoord  //
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,           0.0f, 0.0f, // Lower left corner
+        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f,            0.0f, 2.0f,  // Upper left corner
+        0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f,             2.0f, 2.0f,   // Upper right corner
+        0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f,            2.0f, 0.0f   // Lower right corner
+};
+
+// Indices for vertices order
+GLuint indices[] =
+    {
+        0, 2, 1, // Upper triangle
+        0, 3, 2  // Lower triangle
+};
 
 int main()
 {
@@ -15,23 +32,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLfloat vertices[] =
-        {
-            //     COORDINATES     /        COLORS      /   TexCoord  //
-            -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Lower left corner
-            -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,  // Upper left corner
-            0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,   // Upper right corner
-            0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f   // Lower right corner
-        };
-
-    // Indices for vertices order
-    GLuint indices[] =
-        {
-            0, 2, 1, // Upper triangle
-            0, 3, 2  // Lower triangle
-        };
-
-    GLFWwindow *window = glfwCreateWindow(800, 800, "demo", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "demo", NULL, NULL);
 
     if (window == NULL)
     {
@@ -73,40 +74,43 @@ int main()
 
     VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void *)0);
     VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void *)(3 * sizeof(float)));
-    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void *)(6*sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void *)(6 * sizeof(float)));
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
-
+    //scale uniform from vertex
     GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
     // Texture
-    int widthTmg, heightImg, numColCh;
-    unsigned char *bytes = stbi_load("C:/Users/ROG/Desktop/openGl/vscode_cpp_opengl/src/textures/image.jpg", &widthTmg, &heightImg, &numColCh, 0);
+    //int widthTmg, heightImg, numColCh;
+    //stbi_set_flip_vertically_on_load(true);
+    //unsigned char *bytes = stbi_load("C:/Users/ROG/Desktop/openGl/vscode_cpp_opengl/src/textures/image.jpg", &widthTmg, &heightImg, &numColCh, 0);
 
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    Texture image("C:/Users/ROG/Desktop/openGl/vscode_cpp_opengl/src/textures/image.jpg",GL_TEXTURE_2D,GL_TEXTURE0,GL_RGB,GL_UNSIGNED_BYTE);
+    image.texUnit(shaderProgram,"tex0",0);
+    //GLuint texture;
+    //glGenTextures(1, &texture);
+    //glActiveTexture(GL_TEXTURE0);
+    //glBindTexture(GL_TEXTURE_2D, texture);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
     // float flatColor[] = {1.0f,1.0f,1.0f,1.0f};
     // glTexParameterfv(GL_TEXTURE_2D,GL_TEXTURE_BORDER_COLOR,flatColor);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthTmg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
-    glGenerateMipmap(GL_TEXTURE_2D);
+    //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthTmg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
+    //glGenerateMipmap(GL_TEXTURE_2D);
 
-    stbi_image_free(bytes);
-    glBindTexture(GL_TEXTURE_2D, 0);
+    //stbi_image_free(bytes);
+    //glBindTexture(GL_TEXTURE_2D, 0);
 
-    GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID,"tex0");
-    shaderProgram.Activate();
-    glUniform1f(tex0Uni,0);
+    //GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+    //shaderProgram.Activate();
+    //glUniform1i(tex0Uni, 0);
 
     glClearColor(0.070588f, 0.062745f, 0.054902f, 1.0f);
 
@@ -119,8 +123,8 @@ int main()
         shaderProgram.Activate();
 
         glUniform1f(uniID, 0.5f);
-        glBindTexture(GL_TEXTURE_2D,texture);
-
+        //glBindTexture(GL_TEXTURE_2D, texture);
+        image.Bind();
         VAO1.Bind();
 
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -136,7 +140,8 @@ int main()
     VAO1.Delete();
     VBO1.Delete();
     EBO1.Delete();
-    glDeleteTextures(1, &texture);
+    image.Delete();
+    //glDeleteTextures(1, &texture);
     shaderProgram.Delete();
 
     glfwDestroyWindow(window);
---
+++ src/texture.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <stb/stb_image.h>
+
+#include "shader.h"
+
+class Texture
+{
+public:
+    GLuint ID;
+    GLenum type;
+    Texture(const char *image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType);
+
+    void texUnit(Shader &shader, const char *uniform, GLuint unit);
+    void Bind();
+    // Unbinds a texture
+    void Unbind();
+    // Deletes a texture
+    void Delete();
+};
